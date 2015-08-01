518e3f845855eeb5152813826e157d78c448f068
Akira Sugiura <urasandesu@gmail.com>
2015-08-01 14:28:12 +0900

    Update Prig and Referenced Libraries
    * 01.QuickTour: Update Prig.
    * 02.CallingOriginal: Update Prig.
    * 03.DefaultBehavior: Update Prig and Moq.
    * 04.Generics: Update Prig.
    * 05.NonPublicReplacement: Update Prig.
    * 06.CooperationWithOther: Update Prig, FakeItEasy, Moq and NSubstitute.
    * 07.ProfilersChain: Update Prig and ReportGenerator. OpenCover has [an access violation issue](https://github.com/OpenCover/opencover/pull/330), so I decide that postpone updating it.
    * 08.MolesMigration: Update Prig and Moq.
    * 09.FakesMigration: Update Prig and Moq.
    * 10.JustMockMigration: Update Prig and Moq.
    * 11.IsolatorMigration: Update Prig and Moq.

--- 07.ProfilersChain/runcoverages.ps1
+++ 07.ProfilersChain/runcoverages.ps1
@@ -5,7 +5,7 @@ $env:URASANDESU_PRIG_CURRENT_DIRECTORY = (Resolve-Path .\ProfilersChainTest\bin\
 $env:URASANDESU_PRIG_TARGET_PROCESS_NAME = "nunit-agent"
 
 # Run tests with OpenCover.
-& .\packages\OpenCover.4.5.3723\OpenCover.Console.exe -target:runtests.bat -filter:+[ProfilersChain]*
+& (Resolve-Path .\packages\OpenCover.*\OpenCover.Console.exe).Path -target:runtests.bat -filter:+[ProfilersChain]*
 
 # Format the result by ReportGenerator.
-& .\packages\ReportGenerator.2.1.4.0\ReportGenerator.exe  -reports:results.xml -targetdir:coverage
+& (Resolve-Path .\packages\ReportGenerator.*\tools\ReportGenerator.exe).Path -reports:results.xml -targetdir:coverage
